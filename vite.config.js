7658f9019b35ef54659ff72886225780c67b1570
Vera Li <vera.ilyasova.98@gmail.com>
2026-04-02 20:21:59 +0900

    update project

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/TO-DO/',
   plugins: [react()],
+  base: '/TO-DO/', // https://github.com/VeraI98/TO-DO.git
 })
